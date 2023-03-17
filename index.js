b146ed3b50ca48a6f0a26ab125e3673068a5ad84
İsmail <idincer944@gmail.com>
2023-03-17 17:00:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const people = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(people) {
+    let invitations = [];
+    for(let i = 0; i < people.length; i++) {
+        invitations.push(`Thank you, ${people[i]}, for the wonderful surprise gift!`)
+        console.log(people[i]);
+    }
+    return invitations;
+}
+
+function countDown(n) {
+    for(let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
+writeCards(people);
